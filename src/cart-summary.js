a3f64b85593d9223bc9e85d1327a59977a44fa44
leapon <leapon@gmail.com>
2016-10-22 09:51:10 -0400

    another test added

--- src/cart-summary.js
+++ src/cart-summary.js
@@ -1,7 +1,17 @@
-function CartSummary() {}
+function CartSummary(items) {
+  this._items = items;
+}
 
 CartSummary.prototype.getSubtotal = function() {
+
+  if (this._items.length) {
+    return this._items.reduce(function(subtotal, item) {
+      return subtotal += (item.quantity * item.price);
+    }, 0);
+  }
+  
   return 0;
+
 };
 
 module.exports = CartSummary;
