2aefbbf02936aee350461fa5c8f55a504af8cbc6
Ni Tongyang <ntyaiqingr@gmail.com>
2016-10-22 10:45:29 -0400

    fixed

--- src/life-engine.js
+++ src/life-engine.js
@@ -3,11 +3,13 @@ function LifeEngine() {
 }
 
 LifeEngine.prototype.isCellLiveNext = function(isLiveNow, neighborNumber) {
-  if(neighborNumber > 1 && neighborNumber < 4) {
-    return true;
-  } else {
-    return false;
-  };
+  if (isLiveNow == true){
+    if(neighborNumber > 1 && neighborNumber < 4) {
+      return true;
+    } else {
+      return false;
+    };
+  }
 };
 
 module.exports = LifeEngine;
