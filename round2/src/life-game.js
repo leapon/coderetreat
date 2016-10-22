3b2de3f34beefcaf8a609eef8c066116bc8a7b74
Ni Tongyang <ntyaiqingr@gmail.com>
2016-10-22 11:48:22 -0400

    print all cells

--- round2/src/life-game.js
+++ round2/src/life-game.js
@@ -2,8 +2,15 @@ function LifeGame() {
 }
 
 LifeGame.prototype.next = function(input) {
+  console.log(">>>input:", input);
   var output = [];
-
+  for (i = 0; i < input.length; i++){
+    row = input[i];
+    for (j = 0; j< row.length; j++){
+      cell = row[j];
+      console.log(">>>output:", i,j,cell);
+    };
+  };
   return output;
 };
 
