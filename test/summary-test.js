d856907c3997d5cc7308f7eeec4adbe5ef9db91e
leapon <leapon@gmail.com>
2016-10-22 09:56:42 -0400

    cleanup

--- test/summary-test.js
+++ test/summary-test.js
@@ -3,7 +3,7 @@ var expect = chai.expect; // we are using the "expect" style of Chai
 var CartSummary = require('../src/cart-summary');
 
 describe('CartSummary', function() {
-  
+
   it('getSubtotal() should return 0 if no items are passed in', function() {
     var cartSummary = new CartSummary([]);
     expect(cartSummary.getSubtotal()).to.equal(0);
@@ -23,7 +23,6 @@ describe('CartSummary', function() {
       quantity: 1,
       price: 40
     }]);
-
     expect(cartSummary.getSubtotal()).to.equal(300);
   });
 
